435cf4590ee532e173bbcaf5cdbdefe2e5a919d7
Katrina Owen <katrina.owen@gmail.com>
2013-09-25 07:46:13 -0600

    Rename csv in shopping list

--- shopping_list/test/shopping_list_test.rb
+++ shopping_list/test/shopping_list_test.rb
@@ -5,12 +5,12 @@ require_relative '../lib/shopping_list'
 
 class ShoppingListTest < Minitest::Test
   def test_filename
-    shopping_list = ShoppingList.new("./test/fixtures/shopping_list.csv")
-    assert_equal "./test/fixtures/shopping_list.csv", shopping_list.filename
+    shopping_list = ShoppingList.new("./test/fixtures/items.csv")
+    assert_equal "./test/fixtures/items.csv", shopping_list.filename
   end
 
   def test_load_data
-    shopping_list = ShoppingList.new("./test/fixtures/shopping_list.csv")
+    shopping_list = ShoppingList.new("./test/fixtures/items.csv")
 
     item = shopping_list.items[3]
     assert_equal "cheese", item.name
