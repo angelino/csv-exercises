937aef721ebeda6f877a2b70ffce98feb6fcdcc5
Katrina Owen <katrina.owen@gmail.com>
2013-10-02 11:33:05 -0600

    Update report card tests

--- report_card/test/grade_test.rb
+++ report_card/test/grade_test.rb
@@ -4,17 +4,25 @@ require 'minitest/pride'
 require_relative '../lib/grade'
 
 class GradeTest < Minitest::Test
+  def test_student_name
+    grade = Grade.new(student_name: "Alice Smith")
+    assert_equal "Alice Smith", grade.student_name
+  end
+
   def test_subject
+    skip
     grade = Grade.new(subject: "Programming in Ruby")
     assert_equal "Programming in Ruby", grade.subject
   end
 
   def test_percentage
+    skip
     grade = Grade.new(percentage: "98")
     assert_equal "98", grade.percentage
   end
 
   def test_all_the_things
+    skip
     data = {subject: "Building Compilers", percentage: "76"}
     grade = Grade.new(data)
     assert_equal "Building Compilers", grade.subject
--- report_card/test/report_card_test.rb
+++ report_card/test/report_card_test.rb
@@ -10,9 +10,11 @@ class ReportCardTest < Minitest::Test
   end
 
   def test_load_data
+    skip
     report_card = ReportCard.new("./test/fixtures/grades.csv")
 
     grade = report_card.grades[1]
+    assert_equal "Alice Smith", grade.student_name
     assert_equal "Calculus", grade.subject
     assert_equal "95", grade.percentage
   end
