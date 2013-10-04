2fb48d5bc58c299f1e0a418cab7bffb313ea5026
Katrina Owen <katrina.owen@gmail.com>
2013-10-04 09:41:33 -0600

    Flesh out shopping list

--- shopping_list/test/item_test.rb
+++ shopping_list/test/item_test.rb
@@ -10,23 +10,23 @@ class ItemTest < Minitest::Test
   end
 
   def test_quantity
-    skip
     item = Item.new(quantity: "1 bottle")
-    assert_equal "1 bottle", item.quantity
+    assert_equal 1, item.quantity
   end
 
   def test_unit_price
-    skip
     item = Item.new(unit_price: "12.34")
-    assert_equal "12.34", item.unit_price
+    assert_equal 12.34, item.unit_price
   end
 
-  def test_all_the_things
-    skip
-    data = {name: "guacamole", quantity: "enough", unit_price: "4.50"}
-    item = Item.new(data)
-    assert_equal "guacamole", item.name
-    assert_equal "enough", item.quantity
+  def test_calculate_price
+    item = Item.new(quantity: "5 g", unit_price: "0.12")
+    assert_equal 0.6, item.price
+  end
+
+  def test_calculate_tax
+    item = Item.new(quantity: "3 lb", unit_price: "12.99")
+    assert_in_delta 3.41, item.tax, 0.01
   end
 end
 
--- shopping_list/test/shopping_list_test.rb
+++ shopping_list/test/shopping_list_test.rb
@@ -4,20 +4,41 @@ require 'minitest/pride'
 require_relative '../lib/shopping_list'
 
 class ShoppingListTest < Minitest::Test
+
+  def filename
+    File.absolute_path("../fixtures/items.csv", __FILE__)
+  end
+
+  attr_reader :shopping_list
+  def setup
+    @shopping_list = ShoppingList.new(filename)
+  end
+
   def test_filename
-    shopping_list = ShoppingList.new("path/to/data.csv")
-    assert_equal "path/to/data.csv", shopping_list.filename
+    assert_equal filename, shopping_list.filename
   end
 
   def test_load_data
     skip
-    filename = File.absolute_path("../fixtures/items.csv", __FILE__)
-    shopping_list = ShoppingList.new(filename)
+    item = shopping_list.items[10]
+    assert_equal "pasta", item.name
+    assert_equal 10, item.quantity
+    assert_equal 7.17, item.unit_price
+  end
 
-    item = shopping_list.items[3]
-    assert_equal "cheese", item.name
-    assert_equal "200g", item.quantity
-    assert_equal "3.71", item.unit_price
+  def test_unit_price_cheaper_than
+    skip
+    items = shopping_list.cheaper_than(1.1)
+    assert_equal 11, items.size
+    assert_equal ["champagne", "potatoes", "cauliflower"], items[0..2].map(&:name)
+  end
+
+  def test_unit_price_more_expensive_than
+    skip
+    items = shopping_list.more_expensive_than(9.9)
+    assert_equal 9, items.size
+    expected = ["hummus", "turkey", "mint", "grapefruit", "apples", "basil", "oil", "shortening", "onions"]
+    assert_equal expected, items.map(&:name)
   end
 end
 
