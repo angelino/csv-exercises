87bb383a098a0840bb408be50eb582d6d39e843c
Katrina Owen <katrina.owen@gmail.com>
2013-10-04 16:26:24 -0600

    Flesh out doctor's office exercise

--- doctors_office/test/appointment_test.rb
+++ doctors_office/test/appointment_test.rb
@@ -1,6 +1,8 @@
 gem 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'date'
+require 'time'
 require_relative '../lib/appointment'
 
 class AppointmentTest < Minitest::Test
@@ -8,9 +10,13 @@ class AppointmentTest < Minitest::Test
   end
 
   def test_time
+    # set up an appointment at "2013-04-01 11:30:00 UTC"
+    assert_equal Time.utc(2013, 4, 1, 11, 30), appointment.time
   end
 
-  def test_all_the_things
+  def test_date
+    # set up an appointment at "2013-01-28 13:45:00 UTC"
+    assert_equal Date.new(2013, 01, 28), appointment.date
   end
 end
 
--- doctors_office/test/doctors_office_test.rb
+++ doctors_office/test/doctors_office_test.rb
@@ -9,5 +9,14 @@ class DoctorsOfficeTest < Minitest::Test
 
   def test_load_data
   end
+
+  def test_find_all_appointments_on_a_given_date_ordered_by_time
+  end
+
+  def test_find_all_appointments_for_a_given_patient
+  end
+
+  def test_available_slots_on_a_given_date
+  end
 end
 
