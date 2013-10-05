5427659548623f84511f379c1edaec40fd6f06c7
Katrina Owen <katrina.owen@gmail.com>
2013-10-04 19:32:33 -0600

    WIP

--- report_card/test/grade_test.rb
+++ report_card/test/grade_test.rb
@@ -10,23 +10,120 @@ class GradeTest < Minitest::Test
   end
 
   def test_subject
-    skip
     grade = Grade.new(subject: "Programming in Ruby")
     assert_equal "Programming in Ruby", grade.subject
   end
 
-  def test_percentage
-    skip
-    grade = Grade.new(percentage: "98")
-    assert_equal "98", grade.percentage
+  def test_score
+    grade = Grade.new(score: "98")
+    assert_equal 98, grade.score
   end
 
-  def test_all_the_things
-    skip
-    data = {subject: "Building Compilers", percentage: "76"}
-    grade = Grade.new(data)
-    assert_equal "Building Compilers", grade.subject
-    assert_equal "76", grade.percentage
+  def test_a
+    (94..100).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("A", grade)
+      assert_grade_point(4.0, grade)
+    end
   end
+
+  def test_a_minus
+    (90..93).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("A-", grade)
+      assert_grade_point(3.7, grade)
+    end
+  end
+
+  def test_b_plus
+    (87..89).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("B+", grade)
+      assert_grade_point(3.3, grade)
+    end
+  end
+
+  def test_b
+    (83..86).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("B", grade)
+      assert_grade_point(3.0, grade)
+    end
+  end
+
+  def test_b_minus
+    (80..82).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("B-", grade)
+      assert_grade_point(2.7, grade)
+    end
+  end
+
+  def test_c_plus
+    (77..79).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("C+", grade)
+      assert_grade_point(2.3, grade)
+    end
+  end
+
+  def test_c
+    (73..76).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("C", grade)
+      assert_grade_point(2.0, grade)
+    end
+  end
+
+  def test_c_minus
+    (70..72).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("C-", grade)
+      assert_grade_point(1.7, grade)
+    end
+  end
+
+  def test_d_plus
+    (67..69).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("D+", grade)
+      assert_grade_point(1.3, grade)
+    end
+  end
+
+  def test_d
+    (63..66).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("D", grade)
+      assert_grade_point(1.0, grade)
+    end
+  end
+
+  def test_d_minus
+    (60..62).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("D-", grade)
+      assert_grade_point(0.7, grade)
+    end
+  end
+
+  def test_f
+    (0..59).each do |i|
+      grade = Grade.new(score: i.to_s)
+      assert_grade("F", grade)
+      assert_grade_point(0.0, grade)
+    end
+  end
+
+  private
+
+  def assert_grade(letter, grade)
+    assert_equal letter, grade.letter, "#{grade.score}% should be #{letter}"
+  end
+
+  def assert_grade_point(grade_point, grade)
+    assert_equal grade_point, grade.point, "#{grade.score}% should be #{grade_point}"
+  end
+
 end
 
--- report_card/test/report_card_test.rb
+++ report_card/test/report_card_test.rb
@@ -4,20 +4,56 @@ require 'minitest/pride'
 require_relative '../lib/report_card'
 
 class ReportCardTest < Minitest::Test
+  def filename
+    File.absolute_path("../fixtures/grades.csv", __FILE__)
+  end
+
+  attr_reader :report_card
+  def setup
+    @report_card = ReportCard.new(filename)
+  end
+
   def test_filename
-    report_card = ReportCard.new('path/to/data.csv')
-    assert_equal 'path/to/data.csv', report_card.filename
+    assert_equal filename, report_card.filename
   end
 
   def test_load_data
     skip
-    filename = File.absolute_path("../fixtures/grades.csv", __FILE__)
-    report_card = ReportCard.new(filename)
+    grade = report_card.grades[17]
+    assert_equal "Gwendolyn Hegmann", grade.student_name
+    assert_equal "Dance Company (Audition)", grade.subject
+    assert_equal 69, grade.percentage
+  end
 
-    grade = report_card.grades[1]
-    assert_equal "Alice Smith", grade.student_name
-    assert_equal "Calculus", grade.subject
-    assert_equal "95", grade.percentage
+  def test_grades_for_student
+    skip
+    grades = report_card.for_student("Beulah Maggio")
+    assert_equal 2, grades.size
+
+    french, algebra = grades
+    assert_equal "Algebra I", algebra.subject
+    assert_equal "B-", algebra.letter
+    assert_equal "French", french.subject
+    assert_equal "B-", algebra.letter
+  end
+
+  def test_grades_for_subject
+    skip
+    grades = report_card.for_subject("Biology")
+    assert_equal 22, grades.size
+
+    grade = grades[7]
+    assert_equal "Werner Deckow", grade.student_name
+    assert_equal "F", grade.letter
+    assert_equal "Biology", grade.subject
+  end
+
+  def test_grade_point_average_for_student
+    assert_in_delta 2.22, report_card.grade_point_average("Titus McCullough"), 0.01
+  end
+
+  def test_average_score
+    assert_equal 64, report_card.average_score_in("Biology")
   end
 end
 
